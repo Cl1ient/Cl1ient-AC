b113ce2dfcff63dd20de52a780b53d4fa056507c
Cl1ient <teekzie68@gmail.com>
2025-07-17 18:56:50 +0200

    Ajout de CheckListener

--- src/Cl1ient/Cl1ientAC.php
+++ src/Cl1ient/Cl1ientAC.php
@@ -2,17 +2,24 @@
 
 namespace Cl1ient;
 
+use Cl1ient\checks\combats\reach\ReachA;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use Cl1ient\checks\Checks;
 
 class Cl1ientAC extends PluginBase implements Listener
 {
-    public function onEnable(): void {
-        $this->getLogger()->notice("Plugin activé");
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
 
+
+    public function onEnable(): void{
+        $this->getLogger()->notice("Enabled Cl1ient AC");
+        $this->checks = new Checks($this);
+        $this->checks->loadChecks();
+    }
     public function onDisable(): void {
         $this->getLogger()->notice("Plugin désactivé");
     }
+
+
+
 }
---
+++ src/Cl1ient/checks/Checks.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Cl1ient\checks;
+
+use Cl1ient\checks\combats\reach\ReachA;
+use Cl1ient\listener\CheckListener;
+use pocketmine\plugin\Plugin;
+
+class Checks {
+
+
+    private $checks = [];
+    private $plugin;
+
+    public function __construct(Plugin $plugin) {
+        $this->plugin = $plugin;
+    }
+
+    public function loadChecks(): void {
+        $this->checks[] = new ReachA();
+        // Ajoute ici tous tes autres checks si tu veux
+
+        // Enregistre le listener global
+        $listener = new CheckListener($this);
+        $this->plugin->getServer()->getPluginManager()->registerEvents($listener, $this->plugin);
+    }
+
+    public function getChecks(): array {
+        return $this->checks;
+    }
+}
