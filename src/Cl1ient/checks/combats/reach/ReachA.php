d3e76f992225ddf6641e985ffcd2e8e00f54c9ec
Cl1ient <teekzie68@gmail.com>
2025-07-17 19:53:06 +0200

    Prend en compte le ping

--- src/Cl1ient/checks/combats/reach/ReachA.php
+++ src/Cl1ient/checks/combats/reach/ReachA.php
@@ -8,6 +8,7 @@ use pocketmine\event\Event;
 use pocketmine\event\Listener;
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat;
+use pocketmine\network\mcpe\NetworkSession;
 
 use Cl1ient\utils\Math;
 
@@ -15,17 +16,26 @@ use Cl1ient\utils\Math;
 
 class ReachA
 {
+    public function getCheck() : string
+    {
+        return "REACH";
+    }
+
+    public function getCheckType() : string
+    {
+        return "A";
+    }
     public function checkJustEvent(Event $event): void {
+
         if (!$event instanceof EntityDamageByEntityEvent) return;
 
         $entity = $event->getEntity();
         $damager = $event->getDamager();
-
+        $ping = $damager->getNetworkSession()->getPing();
         //if (!$entity instanceof Player || !$damager instanceof Player) return;
 
-        // Ignorer si un joueur n'est pas en survie
-        if (!$damager->isSurvival() || !$entity->isSurvival()) return;
 
+        //if (!$damager->isSurvival() || !$entity->isSurvival()) return;
 
 
         $locEntity = $entity->getLocation();
@@ -33,9 +43,10 @@ class ReachA
 
         $deltaY = max(0, $locEntity->getY() - $locDamager->getY());
         $distance = Math::distance($locEntity, $locDamager) - $deltaY;
+        $tolerance = ($ping / 1000) * 4 + 0.1;
 
-        if ($distance > 3.2) {
-            $damager->sendMessage(TextFormat::RED . "REACH :: CHECK A");
+        if ($distance > 3.0 + $tolerance){
+            $damager->sendMessage(TextFormat::RED . $this->getCheck() . " ".TextFormat::BLUE . "CHECK " .$this->getCheckType() ." " . TextFormat::RED . $distance . "/3" . "PING : " . $ping );
         }
     }
 
