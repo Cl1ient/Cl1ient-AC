6be57ff22953ea62da8608e340ce030c83c94edc
Cl1ient <109211185+Cl1ient@users.noreply.github.com>
2025-08-22 18:17:33 +0200

    Update Checks.php

--- src/Cl1ient/checks/Checks.php
+++ src/Cl1ient/checks/Checks.php
@@ -18,9 +18,7 @@ class Checks {
 
     public function loadChecks(): void {
         $this->checks[] = new ReachA();
-        // Ajoute ici tous tes autres checks si tu veux
-
-        // Enregistre le listener global
+     
         $listener = new CheckListener($this);
         $this->plugin->getServer()->getPluginManager()->registerEvents($listener, $this->plugin);
     }
